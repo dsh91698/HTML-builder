f283f293021593df85b89c3962b095cb36e21427
dsh91698 <56885229+dsh91698@users.noreply.github.com>
2022-05-18 11:44:01 +0400

    feat: 02-write cntrl-c handler add

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -1,22 +1,35 @@
+// --------------------------------------------------------------------------------------------------------------------
 const { stdin, stdout } = process;
 const fs = require('fs');
 const path = require('path');
-const fileInDir = path.join(__dirname, 'text.txt');
+const fileName = 'text.txt'
+const fileInDir = path.join(__dirname, fileName);
+//--------------------------------------------------------------------------------------------------------------------
 
+// greeting - start -----------------------------------------------------------------------------------------------
 stdout.write('Hello {username} !\n');
+stdout.write('You can type something in terminal and it will be added into the text.txt file\n');
+stdout.write('after you push the Enter button!\n');
+// greeting - finish ----------------------------------------------------------------------------------------------------
 
+// create file stream ----------------------------------------------------------------------------------------------------
 let writeToFileStream = fs.createWriteStream(fileInDir);
 
+// write to file stream -------------------------------------------------------------------------------------------------------
 stdin.on('data', data => {
-    let onExit = data.toString().trim() === 'exit';
-    console.log('data.toString(): ', data.toString());
+    let onExit = data.toString().trim() === 'exit'; //bool flag: TRUE if user typed 'exit' in console
     if (onExit) {
-        writeToFileStream.end();
-        stdin.destroy();
-        process.exit();
+        CntrlCHandler(); // if user typed 'exit' in console, terminate the seance and exit the program
     } else {
         writeToFileStream.write(data)
     }
 });
 
+function CntrlCHandler() {
+    writeToFileStream.end();
+    stdin.destroy();
+    process.exit();
+  }
+
 process.on('exit', () => stdout.write('Удачи {username} в изучении Node.js!'));
+process.on('SIGINT', () => { CntrlCHandler() }); // handle Ctrl+C
