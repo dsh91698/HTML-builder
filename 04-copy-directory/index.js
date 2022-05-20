f264066816af57c208e326b3a28891cc0a95d9ef
dsh91698 <56885229+dsh91698@users.noreply.github.com>
2022-05-20 22:16:32 +0400

    feat: 04 task done, deep copy implemented

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,4 +1,4 @@
-console.log('task copy-folder')
+console.log('# --------------- Copy task START --------------- #');
 
 const fs = require('fs');
 const path = require('path');
@@ -7,64 +7,46 @@ const folderName = 'files'
 const destName = `${folderName}-copy`
 const src = path.join(__dirname, folderName)
 const dest = path.join(__dirname, destName)
-
 copyDir(src, dest)
 
-
-
-async function copyDir() {
+async function copyDir(src, dest) {
     try {
+        console.log('# ------ Copy folder to destination:', dest);
+        await onFolderExists(dest);
+        // await fs.promises.mkdir(dest, { recursive: false});
         await fs.promises.mkdir(dest, { recursive: true });
-        await copyFilesInDir(src);
+        await copyFilesInDir(src, dest);
     } catch (error) {
-        console.log('CopyDir error:', error)
+        console.log('---CopyDir error:', error)
     }
 }
 
-async function copyFilesInDir(dirName) {
+async function copyFilesInDir(dirName, dest) {
     try {
         const files = await fs.promises.readdir(dirName, { withFileTypes: true });
         for (let file of files) {
             if (file.isFile()) {
-                // console.log(`${file.name} - ${path.extname(file.name).slice(1)} - ${fileSize.size} bytes `);
                 await fs.promises.copyFile(path.join(dirName, file.name), path.join(dest, file.name));
+            }
+            else if (file.isDirectory) {
+                // void 0;
+                await copyDir(path.join(dirName, file.name), path.join(dest, file.name));
             }
         }
     } catch (err) {
         console.error('copyFile error:', err);
     }
-} // readFilesInDir(dirName);
+} // copyFilesInDir(dirName);
 
-
-// async function copyDir1() {
-//     const files = await fs.readdir(src)
-//     if (!fs.existsSync(dest)) {
-//         fs.mkdirSync(dest)
-//     }
-//     files.forEach(file => {
-//         const srcPath = path.join(src, file)
-//         const destPath = path.join(dest, file)
-//         if (fs.lstatSync(srcPath).isDirectory()) {
-//         copyDir(srcPath, destPath)
-//         } else {
-//         fs.copyFileSync(srcPath, destPath)
-//         }
-//     })
-// }
-
-
-// function copyDirSync(src, dest) {
-//   const files = fs.readdirSync(src)
-//   if (!fs.existsSync(dest)) {
-//     fs.mkdirSync(dest)
-//   }
-//   files.forEach(file => {
-//     const srcPath = path.join(src, file)
-//     const destPath = path.join(dest, file)
-//     if (fs.lstatSync(srcPath).isDirectory()) {
-//       copyDir(srcPath, destPath)
-//     } else {
-//       fs.copyFileSync(srcPath, destPath)
-//     }
-//   })
-// }
+async function onFolderExists(dirName) { 
+    try {
+        await fs.promises.access(dirName, fs.constants.F_OK);
+        console.log(`# --- Folder exists, removing:\n${dirName}`);
+        await fs.promises.rm(dirName, { recursive: true, force: true });
+        console.log(`# --- Folder removing DONE!`);
+        // The check OK, folder exists
+    } catch (error) {
+        console.log(`# --- Folder ${dirName} DOES NOT exists`);
+        // The check is notOK, folder does not exists
+    }
+}
